ccf43493c4550f88dbe7055bf073e583036e19ac
Alan Oliver <alrobwilloliver@gmail.com>
2020-04-14 16:42:49 +0800

    implemented error handling as middleware in app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const courseRouter = require('./routes/courseRoutes');
 const classRouter = require('./routes/classRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -18,5 +20,14 @@ app.use(express.json());
 app.use('/api/v1/courses', courseRouter);
 app.use('/api/v1/classes', classRouter);
 app.use('/api/v1/users', userRouter);
+
+// error handling
+
+app.all('*', (req, res, next) => {
+    return next(new AppError(`Can't find the url ${req.originalUrl} on this server!`, 404))
+})
+
+app.use(globalErrorHandler);
+
 
 module.exports = app;
