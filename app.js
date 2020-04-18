eb64d57bab0b62fd8ca8ea6826a1f3bd73f24826
Alan Oliver <alrobwilloliver@gmail.com>
2020-04-19 00:10:05 +0800

    Added ejs and views with one nav for home and one for other pages.

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const path = require('path');
 
+
 const app = express();
 
 // Middleware
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname + '/public')))
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -20,6 +24,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 // Routes
+
+app.get('/', (req, res, next) => {
+    res.render('pages/test')
+})
+
 app.use('/api/v1/courses', courseRouter);
 app.use('/api/v1/classes', classRouter);
 app.use('/api/v1/users', userRouter);
