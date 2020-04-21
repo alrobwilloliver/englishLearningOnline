f93f2f630387e0f9c9a1824e056a7a04373667b7
Alan Oliver <alrobwilloliver@gmail.com>
2020-04-22 03:18:14 +0800

    Added classes and course route with classes and class page templates

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -10,35 +10,45 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.get('/login', (req, res, next) => {
-    res.render('pages/login')
-})
+// router.get('/login', (req, res, next) => {
+//     res.render('pages/login')
+// })
 
 // router.get('/signup', (req, res, next) => {
 //     res.render('pages/signup')
 // })
 
-router.get('/classes/:courseId', catchAsync(async (req, res, next) => {
+router.get('/courses/:courseId', catchAsync(async (req, res, next) => {
     let filter = {};
 
     if (req.params.courseId) filter = { course: req.params.courseId }
 
     const classes = await Class.find(filter)
 
-    const links = classes.map(el => {
-        return `/class/${el._id}`
-    })
-
     res.render('pages/classes', {
         status: 'success',
         results: classes.length,
         data: classes,
-        links
     })
 }))
 
 router.get('/classes', (req, res, next) => {
     res.render('pages/classes')
 })
+
+router.get('/courses/:courseId/classes/:classId', catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (!req.params.courseId || !req.params.classId) next(new AppError('This record doesn\'t exist!', 500));
+
+    filter = { $and: [{ _id: req.params.classId }, { course: req.params.courseId }] }
+
+    const lesson = await Class.find(filter);
+    console.log(lesson)
+
+    res.render('pages/class', {
+        status: 'success',
+        data: lesson
+    })
+}))
 
 module.exports = router;
