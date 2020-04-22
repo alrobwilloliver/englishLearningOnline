fae8cd2bcd0bc5d186b599a6576e360163b8400f
Alan Oliver <alrobwilloliver@gmail.com>
2020-04-22 16:39:01 +0800

    View Controller refactor of view routes

---
+++ controllers/viewController.js
@@ -0,0 +1,37 @@
+const AppError = require('../utils/appError');
+const Class = require('../models/classModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getHome = (req, res, next) => {
+    res.render('pages/index', {
+        title: 'Home'
+    })
+}
+
+exports.getOneOfCourse = catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (!req.params.courseId || !req.params.classId) next(new AppError('This record doesn\'t exist!', 500));
+
+    filter = { $and: [{ _id: req.params.classId }, { course: req.params.courseId }] }
+
+    const lesson = await Class.findOne(filter);
+
+    res.render('pages/class', {
+        status: 'success',
+        data: lesson
+    })
+})
+
+exports.getAllOfCourse = catchAsync(async (req, res, next) => {
+    let filter = {};
+
+    if (req.params.courseId) filter = { course: req.params.courseId }
+
+    const classes = await Class.find(filter)
+
+    res.render('pages/classes', {
+        status: 'success',
+        results: classes.length,
+        data: classes,
+    })
+})
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const Class = require('../models/classModel');
 const catchAsync = require('../utils/catchAsync');
+const viewController = require('../controllers/viewController');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('pages/index', {
-        title: 'Home'
-    })
-})
+router.get('/', viewController.getHome)
 
 // router.get('/login', (req, res, next) => {
 //     res.render('pages/login')
@@ -18,37 +15,12 @@ router.get('/', (req, res, next) => {
 //     res.render('pages/signup')
 // })
 
-router.get('/courses/:courseId', catchAsync(async (req, res, next) => {
-    let filter = {};
-
-    if (req.params.courseId) filter = { course: req.params.courseId }
-
-    const classes = await Class.find(filter)
-
-    res.render('pages/classes', {
-        status: 'success',
-        results: classes.length,
-        data: classes,
-    })
-}))
+router.get('/courses/:courseId', viewController.getAllOfCourse)
 
 router.get('/classes', (req, res, next) => {
     res.render('pages/classes')
 })
 
-router.get('/courses/:courseId/classes/:classId', catchAsync(async (req, res, next) => {
-    let filter = {};
-    if (!req.params.courseId || !req.params.classId) next(new AppError('This record doesn\'t exist!', 500));
-
-    filter = { $and: [{ _id: req.params.classId }, { course: req.params.courseId }] }
-
-    const lesson = await Class.find(filter);
-    console.log(lesson)
-
-    res.render('pages/class', {
-        status: 'success',
-        data: lesson
-    })
-}))
+router.get('/courses/:courseId/classes/:classId', viewController.getOneOfCourse)
 
 module.exports = router;
