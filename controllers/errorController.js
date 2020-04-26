1fe8a0480b7e77f24b3f0de2a7267984366ec6f1
Alan Oliver <alrobwilloliver@gmail.com>
2020-04-26 14:16:57 +0100

    Added error page and linked error handling to front end route

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -22,33 +22,66 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 }
 
-const sendErrDev = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
-    });
-}
-
-const sendErrProd = (err, res) => {
-    // Operational, trusted error: send message to client
-    if (err.isOperational) {
+const sendErrDev = (err, req, res) => {
+    // API
+    if (req.originalUrl.startsWith('/api')) {
         res.status(err.statusCode).json({
             status: err.status,
+            error: err,
             message: err.message,
+            stack: err.stack
         });
-
-        //Programming or other unknown error: don't leak error details
     } else {
-        // 1 Log Error
+        // Rendered Website
         console.error('ERROR:', err);
 
-        // 2 response to client
-        res.status(500).json({
-            status: 'error',
-            message: 'Something went wrong!'
+        res.status(err.statusCode).render('pages/error', {
+            title: 'Error!',
+            msg: err.message
         })
+    }
+}
+
+const sendErrProd = (err, req, res) => {
+    // A. API
+    if (req.originalUrl.startsWith('/api')) {
+        // Operational, trusted error: send message to client
+        if (err.isOperational) {
+            res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message,
+            });
+
+            //Programming or other unknown error: don't leak error details
+        } else {
+            // 1 Log Error
+            console.error('ERROR:', err);
+
+            // 2 response to client
+            res.status(500).json({
+                status: 'error',
+                message: 'Something went wrong!'
+            })
+        }
+    } else {
+        // B. Rendered Website
+        if (err.isOperational) {
+            res.status(err.statusCode).render('pages/error', {
+                title: 'Error!',
+                msg: err.message
+            })
+
+            //Programming or other unknown error: don't leak error details
+        } else {
+            // 1 Log Error
+            console.error('ERROR:', err);
+
+            // 2 response to client
+            res.status(err.statusCode).render('pages/error', {
+                title: 'Error!',
+                msg: 'Please try again later.'
+            })
+        }
     }
 }
 
@@ -57,9 +90,10 @@ module.exports = (err, req, res, next) => {
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrDev(err, res);
+        sendErrDev(err, req, res);
     } else if (process.env.NODE_ENV === 'production') {
         let error = { ...err };
+        error.message = err.message
 
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
@@ -67,6 +101,6 @@ module.exports = (err, req, res, next) => {
         if (error.name === 'JsonWebTokenError') error = handleJWTError();
         if (error.name === 'TokenExpiredError') error = handleJWTExpired();
 
-        sendErrProd(error, res);
+        sendErrProd(error, req, res);
     }
 };
