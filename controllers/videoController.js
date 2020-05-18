1e6f174a814c37c21f2d2dd3f75e8229841bb161
Alan Oliver <alrobwilloliver@gmail.com>
2020-05-18 23:18:09 +0100

    Moving to GridFSBucket for streaming. Still encountering errors.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,26 +1,28 @@
 const mongoose = require('mongoose');
 const Grid = require('gridfs-stream');
 const catchAsync = require('../utils/catchAsync');
+const mongodb = require('mongodb');
 
 exports.getVideo = catchAsync(async (req, res, next) => {
-    const gfs = Grid(mongoose.connection.db, mongoose.mongo)
+    let bucket = new mongodb.GridFSBucket(mongoose.connection.db)
+    // const gfs = Grid(mongoose.connection.db, mongoose.mongo)
+
+    const name = 'Lecture-10-Video.mp4';
 
     const range = req.headers.range;
-    // console.log(req.headers);
     if (range) {
+        console.log(range);
         const fileSize = 355190999;
-        const chunksize = 261120;
-        const name = 'Lecture-10-Video.mp4';
-
-        const readStream = gfs.createReadStream(name);
-
-        // console.log(readStream);
-
 
         const parts = range.replace(/bytes=/, "").split("-")
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+        const start = parseInt(parts[0], 10)
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize - 1
+        const chunksize = (end - start) + 1;
 
+        // const readStream = gfs.createReadStream(name);
+        let downloadStream = bucket.openDownloadStreamByName(name, { start, end })
 
         const head = {
             'Content-Range': `bytes ${start}-${end}/${fileSize}`,
@@ -30,16 +32,43 @@ exports.getVideo = catchAsync(async (req, res, next) => {
         }
 
         res.writeHead(206, head);
-        readStream.pipe(res);
+
+        downloadStream.on('data', (chunk) => {
+            res.write(chunk);
+        })
+
+        downloadStream.on('error', () => {
+            res.sendStatus(404);
+        })
+
+        downloadStream.on('end', () => {
+            res.end();
+        })
+        // readStream.pipe(res);
+
+
+
     } else {
         const fileSize = 355190999;
         const head = {
             'Content-Length': fileSize,
             'Content-Type': 'video/mp4',
         }
-        res.writeHead(200, head)
-        gfs.createReadStream('Lecture-10-Video.mp4').pipe(res);
-    }
+        let downloadStream = bucket.openDownloadStreamByName(name);
 
-    // readStream.pipe(res);
+        downloadStream.on('data', () => {
+            res.writeHead(200, head);
+            res.write();
+        })
+        // gfs.createReadStream('Lecture-10-Video.mp4').pipe(res);
+
+        downloadStream.on('error', () => {
+            res.sendStatus(404);
+        });
+
+        downloadStream.on('end', () => {
+            res.end();
+        });
+
+    }
 })
