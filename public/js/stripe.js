2d97274bad23831d702fa45d8906b944d154d626
alrobwilloliver <alrobwilloliver@gmail.com>
2021-11-06 13:57:44 +0000

    fix url

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -3,7 +3,9 @@ var stripe = Stripe('pk_test_51HAOUmLILjZE998n6MxXIoJhGknhAxemcuDDW7VNBTzkYgUxHz
 const bookCourse = async courseId => {
     try {
         // 1) get the session from the server (api)
-        const url = window.location.origin
+        const production  = 'https://limitless-eyrie-17644.herokuapp.com/';
+        const development = 'http://localhost:4000/';
+        const url = (process.env.NODE_ENV ? production : development)
         const session = await axios(url + `api/v1/bookings/checkout-session/${courseId}`)
         // 2) Create checkout form + charge credit card for us
         // console.log(session);
